956628b0c23819519d4b27367da2b54181cc5195
wolfpack <wolfpack@vulk.co>
2015-07-24 16:59:57 -0400

    added controller template for testing

--- lib/generators/api_generator.rb
+++ lib/generators/api_generator.rb
@@ -1,12 +1,23 @@
 class ApiGenerator < Rails::Generators::NamedBase
-  def create_initializer_file
+  def create_model_file
     create_file "app/models/#{file_name}.rb", <<-FILE 
-      class #{class_name} < ActiveRecord::Base
-        
-        def as_json options = { } 
-          {}
-        end 
-      end 
+  class #{class_name} < ActiveRecord::Base
+
+    def as_json options = { } 
+      {}
+    end 
+  end 
     FILE
   end
+
+  def create_controller_file
+    create_file "app/controllers/#{plural_name}_controller.rb", <<-FILE
+  class #{plural_name}Controller < ApplicationController
+
+    def show
+      @#{plural_name} = #{class_name}.find(params[:id])
+      render json: {:#{plural_name} => @#{plural_name}}
+    end
+  end 
+   FILE  
 end
